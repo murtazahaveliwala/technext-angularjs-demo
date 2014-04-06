520df866e7c4a58405cbfb005b9f679ad38d34c4
Murtaza Haveliwala <murtaza.haveliwala@synerzip.com>
2014-04-07 02:04:29 +0530

    Renamed AngularApp-Todo directory to static. Updated server script as well
    Change-Id: Ic51f01c699e05d0545ca05f84082656fb429a7ce

--- demos/server.js
+++ demos/server.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + '/AngularApp-Todo'));
+app.use(express.static(__dirname + '/static'));
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000, function(err) {
+	if (err) {
+		console.log('Server couldn\'t be started!', err);
+	} else {
+		console.log('Server started.');
+	}
+});
