8513667afbe707d6f6775747a4edd8284463f74f
Murtaza Haveliwala <murtaza.haveliwala@synerzip.com>
2014-04-10 01:51:35 +0530

    1. Added an demo example for tabs directive 2. Updated presentation with a demo link. TODO: Update them with working links for all demos
    Change-Id: Iad9cc3001c9d7d909452b78c5c60a08b2f698452

---
+++ demos/static/angular-apps/final/my-directive/js/tabsDirectiveDemo.js
@@ -0,0 +1,46 @@
+var app = angular.module('tabsDirectiveDemo', []);
+
+app.directive('tabs', function() {
+  return {
+    restrict: 'E',
+    templateUrl: 'partials/tabs.html',
+    replace: true,
+    transclude: true,
+    scope: {},
+    controller: function($scope) {
+      var activeTab = null;
+      $scope.tabsList = [];
+
+      $scope.showMe = function(tab) {
+        activeTab.selected = false;
+        tab.selected = true;
+        activeTab = tab;
+      };
+
+      this.add = function(tab) {
+        tab.selected = false;
+        $scope.tabsList.push(tab);
+        if ($scope.tabsList.length === 1) {
+          tab.selected = true;
+          activeTab = tab;
+        }
+      }
+    }
+  };
+});
+
+app.directive('tab', function() {
+  return {
+    restrict: 'E',
+    template: '<div ng-class="{\'active\': selected}" ng-show="selected" ng-transclude></div>',
+    replace: true,
+    transclude: true,
+    scope: {
+      title: '@'
+    },
+    require: '^tabs',
+    link: function(scope, element, attrs, tabsController) {
+      tabsController.add(scope);
+    }
+  };
+});
